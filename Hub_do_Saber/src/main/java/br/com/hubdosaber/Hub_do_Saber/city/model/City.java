8a8f7005a7f96f930684efdf643b3d711c659095
Alicia <alicia.feliciano@zup.com.br>
2025-07-29 22:53:18 -0300

    Criação da model, repository e state

---
+++ Hub_do_Saber/src/main/java/br/com/hubdosaber/Hub_do_Saber/city/model/City.java
@@ -0,0 +1,53 @@
+package br.com.hubdosaber.Hub_do_Saber.city.model;
+
+
+import br.com.hubdosaber.Hub_do_Saber.city.repository.State;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "city")
+public class City {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    private State state;
+
+    public City() {
+    }
+
+    public City(String name, State state) {
+        this.name = name;
+        this.state = state;
+    }
+
+    // Getters
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public State getState() {
+        return state;
+    }
+
+    // Setters
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setState(State state) {
+        this.state = state;
+    }
+}
